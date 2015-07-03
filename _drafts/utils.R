1c79785306996f949954be60a560f5d45bebfed2
Siddarta Jairam <sidjsb@gmail.com>
2015-07-03 18:53:11 -0500

    made a function to do the publishing
    -tried to do hooks but couldn't get them right.

---
+++ _drafts/utils.R
@@ -0,0 +1,53 @@
+getJkylYaml <- function(layout, title, ...){
+	addList <- c(layout = layout, title = title, list(...))
+	addYaml <- paste0(names(addList), ": ", addList)
+	addYaml <- c("---", addYaml, "---")
+
+	return(addYaml)
+}
+
+publishGitio <- function(path, layout, tags){
+	if(grepl("[.]rmd", path)){
+		knitr::render_jekyll(highlight = "pygments")
+		rmarkdown::render(path)
+		rmdPath <- path
+		mdPath <- gsub("[.]rmd", ".md", path)
+	} else {
+		rmdPath <- gsub("[.]md", ".rmd", path)
+		mdPath <- path
+	}
+
+	blogRoot <- dirname(dirname(path))
+	pubMd <- blogMd <- readLines(mdPath)
+
+	blogRmd <- readLines(rmdPath)
+
+	normTitle <- grabYamlOpt(rmdPath, "title")
+	normAuthor <- grabYamlOpt(rmdPath, "author")
+
+	postFileName <- paste0(strftime(Sys.time(), '%Y-%m-%d-'),
+												 gsub(" ", '-', normTitle))
+	pathPub <- paste(blogRoot, "_posts", postFileName , sep = '/')
+
+	jYaml <- getJkylYaml(layout, title = normTitle, author = normAuthor, tags = tags )
+
+	pubMd <- c(jYaml, pubMd)
+	writeLines(pubMd, pathPub)
+
+	#build with jekyll
+	#system(paste("cd", paste0('"', blogRoot, '"'), "&&", "jekyll build"))
+
+
+}
+
+grabYamlOpt <- function(path, option){
+	if( length(path) == 1){
+		lines <- readLines(path)
+	} else {
+		lines <- path
+	}
+	yOpt <- paste0(option, ": ")
+	verbOpt <- gsub(yOpt, '', grep(yOpt, lines, value = TRUE))
+
+	return( gsub('"', '', verbOpt))
+}
